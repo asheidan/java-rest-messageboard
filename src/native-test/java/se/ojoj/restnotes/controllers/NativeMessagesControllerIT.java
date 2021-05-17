8c504f873494e35d8a8a4d5cf2330193389c2f61
Emil Eriksson <asheidan@gmail.com>
2021-05-17 16:57:26 +0200

    Initial commit
    Files auto generated by quarkus-maven-plugin

---
+++ src/native-test/java/se/ojoj/restnotes/controllers/NativeMessagesControllerIT.java
@@ -0,0 +1,9 @@
+package se.ojoj.restnotes.controllers;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMessagesControllerIT extends MessagesControllerTest {
+
+    // Execute the same tests but in native mode.
+}
