ee5babb6063b9b606d749798203f96481bcfeab4
Emil Eriksson <asheidan@gmail.com>
2021-05-18 09:17:45 +0200

    Add post/read scenario for messages

--- src/test/java/se/ojoj/restnotes/integrationtests/MessagesScenarioTest.java
+++ src/test/java/se/ojoj/restnotes/integrationtests/MessagesScenarioTest.java
@@ -1,9 +1,14 @@
 package se.ojoj.restnotes.integrationtests;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -14,7 +19,7 @@ public class MessagesScenarioTest {
     given()
         .when().get("/messages")
         .then()
-        .statusCode(200);
+          .statusCode(200);
   }
 
   @Test
@@ -22,8 +27,27 @@ public class MessagesScenarioTest {
     given()
         .when().get("/messages")
         .then()
-        .statusCode(200)
-        .body(is("[]"));
+          .statusCode(200)
+          .body(is("[]"));
+  }
+
+  @Test
+  @TestTransaction
+  public void testPostedMessageShouldBeReturned() {
+    String createResult = with()
+        .contentType(ContentType.JSON)
+        .body("{\"body\": \"Lorem ipsum doler sit amet.\"}")
+        .post("/messages")
+        .asString();
+
+    Integer messageId = JsonPath.from(createResult).get("id");
+
+    with()
+        .when().get("/messages/" + messageId)
+        .then()
+          .statusCode(200)
+          .body("body", is("Lorem ipsum doler sit amet."));
+
   }
 
 }
